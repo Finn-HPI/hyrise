6bf0fc08bf08bfbb38ad3dde1df8051f80c38966
Finn Schoellkopf <schoellkopf.finn@gmail.com>
2025-03-09 10:09:47 +0100

    change BLOOM_FILTER_SIZE to unsigned type

--- src/lib/operators/join_hash/join_hash_steps.hpp
+++ src/lib/operators/join_hash/join_hash_steps.hpp
@@ -246,7 +246,7 @@ class PosHashTable {
 // Some of these points could be addressed with relatively low effort and should bring additional, significant benefits.
 // We did not yet work on this because the Bloom filter was a byproduct of a research project and we have not had the
 // resources to optimize it at the time.
-static constexpr auto BLOOM_FILTER_SIZE = 1 << 20;
+static constexpr auto BLOOM_FILTER_SIZE = 1u << 20u;
 static constexpr auto BLOOM_FILTER_MASK = BLOOM_FILTER_SIZE - 1;
 
 // Using dynamic_bitset because, different from vector<bool>, it has an efficient operator| implementation, which is
