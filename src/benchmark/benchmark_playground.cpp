fab652ef2de86a5d91b0966d09eed89f43998388
Finn Schoellkopf <schoellkopf.finn@gmail.com>
2025-05-04 10:40:07 +0200

    modify mway merge bench

--- src/benchmark/benchmark_playground.cpp
+++ src/benchmark/benchmark_playground.cpp
@@ -11,6 +11,7 @@
 
 #include "operators/join_simd_sort_merge/k_way_merge.hpp"
 #include "operators/join_simd_sort_merge/multiway_merging.hpp"
+#include "operators/join_simd_sort_merge/multiway_merging_balkesen.hpp"
 #include "operators/join_simd_sort_merge/radix_partitioning.hpp"
 #include "operators/join_simd_sort_merge/simd_utils.hpp"
 #include "operators/join_simd_sort_merge/util.hpp"
@@ -50,6 +51,9 @@ namespace {
 }
 }  // namespace
 
+constexpr auto CACHE_SIZE = size_t{1024} * 1024 * 1;
+constexpr auto CACHE_PER_THREAD = CACHE_SIZE / 1;
+
 // NOLINTNEXTLINE
 static void BM_MWAY_MERGE(benchmark::State& state) {
   const auto fan_in = static_cast<size_t>(state.range(0));
@@ -61,25 +65,22 @@ static void BM_MWAY_MERGE(benchmark::State& state) {
   // NOLINTNEXTLINE
   for (auto _ : state) {
     state.PauseTiming();
-    // std::cout << "start setup\n";
-
     auto chunks = std::vector<simd_sort::simd_vector<SimdElement>>(fan_in);
-    auto buckets = std::vector<radix_partition::Bucket>(fan_in);
-    auto sorted_bucket_ptrs = std::vector<Bucket*>(fan_in);
-
+    auto buckets = std::vector<Relation>(fan_in);
+    auto sorted_bucket_ptrs = std::vector<Relation*>(fan_in);
     for (auto chunk_id = size_t{0}; chunk_id < fan_in; ++chunk_id) {
       chunks[chunk_id] = generate_ordered_tuples(chunk_size);
-      buckets[chunk_id].data = chunks[chunk_id].data();
-      buckets[chunk_id].size = chunk_size;
+      buckets[chunk_id].tuples = chunks[chunk_id].data();
+      buckets[chunk_id].num_tuples = chunk_size;
       sorted_bucket_ptrs[chunk_id] = &buckets[chunk_id];
     }
     auto merged_output = simd_sort::simd_vector<SimdElement>(chunk_size * fan_in);
-    // std::cout << "finished setup\n";
     state.ResumeTiming();
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    auto multiway_merger = multiway_merging::MultiwayMerger<choose_count_per_vector(), SortingType>(sorted_bucket_ptrs);
+    auto multiway_merger = multiway_merging::MultiwayMergerBalkesen<choose_count_per_vector(), SortingType>(
+        sorted_bucket_ptrs, CACHE_PER_THREAD);
     multiway_merger.merge(merged_output);
     benchmark::DoNotOptimize(merged_output);
     benchmark::ClobberMemory();
@@ -104,12 +105,9 @@ static void BM_MWAY_MERGE(benchmark::State& state) {
   // NOLINTNEXTLINE
   for (auto _ : state) {
     state.PauseTiming();
-    // std::cout << "start setup\n";
-
     auto chunks = std::vector<simd_sort::simd_vector<SimdElement>>(fan_in);
     auto buckets = std::vector<radix_partition::Bucket>(fan_in);
     auto sorted_bucket_ptrs = std::vector<Bucket*>(fan_in);
-
     for (auto chunk_id = size_t{0}; chunk_id < fan_in; ++chunk_id) {
       chunks[chunk_id] = generate_ordered_tuples(chunk_size);
       buckets[chunk_id].data = chunks[chunk_id].data();
@@ -117,7 +115,6 @@ static void BM_MWAY_MERGE(benchmark::State& state) {
       sorted_bucket_ptrs[chunk_id] = &buckets[chunk_id];
     }
     auto merged_output = simd_sort::simd_vector<SimdElement>(chunk_size * fan_in);
-    // std::cout << "finished setup\n";
     state.ResumeTiming();
 
     auto start = std::chrono::high_resolution_clock::now();
